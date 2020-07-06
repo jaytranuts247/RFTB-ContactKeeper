3ff7f81e8844ef6fb00f183bb1f34f3108035efb
themikaelson <34260590+themikaelson@users.noreply.github.com>
2020-07-06 19:06:50 -0400

    update contacts.js

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
 
 const Contacts = (props) => {
+	const contactContext = useContext(ContactContext);
+	const { contacts } = contactContext;
 	return (
 		<Fragment>
-			{props.contacts.map((contact) => (
+			{contacts.map((contact) => (
 				<ContactItem key={contact.id} contact={contact} />
 			))}
 		</Fragment>
