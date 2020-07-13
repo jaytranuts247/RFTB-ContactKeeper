974999395bc4910721d7453ae5bd3420a47e2077
themikaelson <34260590+themikaelson@users.noreply.github.com>
2020-07-13 17:07:08 -0400

    add Procfile

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -61,7 +61,7 @@ const ContactState = (props) => {
 	// Delete Contact
 	const deleteContact = async (id) => {
 		try {
-			const res = await axios.delete(`/api/contacts/${id}`);
+			await axios.delete(`/api/contacts/${id}`);
 			dispatch({ type: DELETE_CONTACT, payload: id });
 		} catch (err) {
 			console.log("err contact add", err);
